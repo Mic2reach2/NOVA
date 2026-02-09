b7e0bf07ba99d50823b962bc6d7f2052986cfc0b
Micheal O. Adetayo <micealad@gmail.com>
2026-02-10 00:07:53 +0100

    Delete Nova Vision Eye Clinic Web Project directory

--- Nova Vision Eye Clinic Web Project/js/newsletter.php
+++
@@ -1,9 +0,0 @@
-<?php
-$email = $_POST['email'];
-
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    file_put_contents("subscribers.txt", $email . PHP_EOL, FILE_APPEND);
-    echo "Thank you for subscribing!";
-} else {
-    echo "Invalid email address.";
-}
